63212298cd6e5440602eaa0402f56309bcaf4741
Adam <adam.m.thompson@icloud.com>
2018-04-26 18:55:35 -0400

    more style fixes & added zoom

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 
 const Colors = ["#ffdaf6","#ffdce5", "#fee2d5", "#ffeab6", "#d1f7c4", "#d0f0fd", "#cfdfff", "#ede2fe", "#f0f0f0"]
+const MillisecsPerDay = 1000 * 60 * 60 * 24;
 
 function dateDiff(d1,d2){
-	return Math.floor( (d2 - d1)/(1000 * 60 * 60 * 24) ) ;
+	return Math.floor( (d2 - d1)/(MillisecsPerDay) ) ;
+}
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
 }
 
 export class TimelineContainer extends React.Component {
 	render() {
+		var Items = JSON.parse(JSON.stringify(this.props.itemsArray))
+		var ReverseItems = JSON.parse(JSON.stringify(this.props.itemsArray))
+		Items.sort(dynamicSort("start"))
+		ReverseItems.sort(dynamicSort("-end"))
+		const firstDate = Items[0].start
+		const lastDate = ReverseItems[0].end
+		console.log(firstDate, lastDate)
 
-		let numItems = this.props.itemsArray.length
-		// TODO: Check to be sure this is true
-		let firstDate = this.props.itemsArray[0].start // We assume the list is pre-sorted, and the first item is the first event
-		
+		const numItems = Items.length
 		var zoom = this.props.zoom
-		var colWidth = zoom * 60
+		var colWidth = zoom * 30
+
+		// Calculate timeline header labels
+		const charsPerLabel = 6 // ex. "Dec 31".length
+		var numCols = dateDiff(Date.parse(firstDate), Date.parse(lastDate))
+		var colsPerLabel = 2 * Math.ceil(4/zoom)
+		console.log("zoom", zoom)
+		var DateLabelsArray = []
+		for (var d = Date.parse(firstDate) + MillisecsPerDay; d < Date.parse(lastDate); d += colsPerLabel * MillisecsPerDay ) {
+			let dt = new Date(d)
+			DateLabelsArray.push(dt.toString().substr(4,charsPerLabel))
+		}
+		console.log(DateLabelsArray)
+
 
 		// Dynamically set the column width based on the zoom level
 		let containerStyle = {
@@ -25,7 +55,11 @@ export class TimelineContainer extends React.Component {
 		return(
 			<div className="timeline-container" style={containerStyle}>
 				{
-					this.props.itemsArray.map(
+					DateLabelsArray.map(
+						(date, idx) => <TimelineLabel date={date} index={idx} columns={colsPerLabel}></TimelineLabel>
+					)
+				}{
+					Items.map(
 						(item, idx) =>  <TimelineEvent item={item} index={idx} firstDate={firstDate} colWidth={colWidth}></TimelineEvent>
 					)
 				}
@@ -34,7 +68,7 @@ export class TimelineContainer extends React.Component {
 	}
 }
 
-export class TimelineEvent extends React.Component {
+class TimelineEvent extends React.Component {
 	render() {
 		const fontSize = 14 // we set the font size here since we need it to approximate the text length
 		let firstDate = Date.parse(this.props.firstDate)
@@ -93,9 +127,21 @@ export class TimelineEvent extends React.Component {
 	}
 }
 
+function TimelineLabel(props) {
+	// const LabelBGColors = ["#ffffff", "#eeeeee"]
+	let LabelStyle = {
+		gridColumnStart: props.index * props.columns + 1,
+		gridColumnEnd: props.index * props.columns + props.columns + 1,
+		// backgroundColor: LabelBGColors[props.index%2]
+	}
+	return(
+		<div className="timeline-label" style={LabelStyle}>{props.date}</div>
+	)
+}
+
 function RegularTimelineElement(props) {
 return(
-	<div className="timeline-event-regular" style={props.style}>
+	<div className="timeline-event timeline-event-regular" style={props.style}>
 		<span className="timeline-event-name">{String(props.text)}</span>
 	</div>
 )}
@@ -103,7 +149,7 @@ return(
 function LongTextTimelineElement(props) {
 
 	return(
-	<div className="timeline-event-long-text" style={props.styles[0]}>
+	<div className="timeline-event timeline-event-long-text" style={props.styles[0]}>
 		<div className="timeline-event-pill" style={props.styles[1]}></div>
 		<span className="timeline-event-name" style={props.styles[2]}>{String(props.text)}</span>
 	</div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Button from 'material-ui/Button';
-import Grid from 'material-ui/Grid';
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-import { TimelineContainer, TimelineEvent } from './components/Timeline.js'
+import { TimelineContainer } from './components/Timeline.js'
 import './style/timelineStyle.css';
 import timelineItems from './timelineItems';
 
+class Timeline extends React.Component {
+
+	constructor(props) {
+	    super(props);
+	    this.handleZoomChange = this.handleZoomChange.bind(this);
+	    this.state = {zoom: 2};
+	  }
+
+	  handleZoomChange(value) {
+	    this.setState({zoom: value});
+	  }
+
+	render(){
+
+		return(
+			<div className="timeline-app">
+			<TimelineContainer itemsArray={this.props.itemsArray} zoom={this.state.zoom} ></TimelineContainer>
+				<div className="slider-container">
+					<label>Zoom: {this.state.zoom}</label>
+					<Slider 
+						className="zoom-slider" 
+						defaultValue={this.state.zoom} 
+						min={1} max={5} 
+						onChange={this.handleZoomChange}
+						// railStyle={{backgroundColor: "#cfdfff"}}
+						handleStyle={[{backgroundColor: "#2d7ff9", borderColor: "#2d7ff9"}, {backgroundColor: "#2d7ff9", boxShadow: "none"}]}
+						trackStyle={[{backgroundColor: "#cfdfff"}, {backgroundColor: "#eeeeee"}]}
+					/>
+				</div>
+			</div>
+		)
+	}
+}
+
 const App = () => (
 <div>
-	<h1>Airtable Timeline{'\u2728'}</h1>
-	<TimelineContainer itemsArray={timelineItems} zoom="1" ></TimelineContainer>
+	<AppBar position="static" color="secondary">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Airtable Timeline
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Timeline itemsArray={timelineItems} />
 </div>
 );
 
